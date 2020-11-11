25b71f0036a352cbf0a1d4c8d9c7ee1b3c934d45
ron <rinbar70@gmail.com>
2020-11-11 23:46:45 +0200

    Add missing HTML document structure.

--- ul.php
+++ ul.php
@@ -1,8 +1,21 @@
-<ul>
-    <?php
-    $arr = ['a', 'b', 'c', 'd'];
-    for ($i = 0; $i < sizeof($arr); $i++) {
-        echo "<li>$arr[$i]</li>";
-    }
-    ?>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unordered List</title>
+</head>
+
+<body>
+    <ul>
+        <?php
+        $arr = ['a', 'b', 'c', 'd'];
+        for ($i = 0; $i < sizeof($arr); $i++) {
+            echo "<li>$arr[$i]</li>";
+        }
+        ?>
+    </ul>
+</body>
+
+</html>
